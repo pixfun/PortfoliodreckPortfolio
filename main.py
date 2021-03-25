25341bdf5317045e6725ca69f50862e667863fe0
positivegossip <73178112+positivegossip@users.noreply.github.com>
2021-03-25 17:13:34 +0100

    Kommentare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle                                       #Library turtle import
 import pygame                                       #Library pygame import
 
-#Festlegen der globalen Variablen (123456)
+#Festlegen der globalen Variablen
 window = turtle.Screen()
 window.bgcolor("black")
 window.title("Tiefensuche im Labyrinth")
