72ea60b1d2c5d5feb91ba16e68c2e2a6755ec221
Sebastian Richter <richters@dhbw-loerrach.de>
2021-03-25 16:54:40 +0100

    test

--- main (1).py
+++ main (1).py
@@ -1,63 +1,13 @@
-#Importieren der Libaries turtle & pygame mit den jeweiligen Funktionen
 import turtle
-import pygame
-
-def waendeLesen():
-    grid = open("maze.txt", "r")
-    xDim = int(f.readline())  # 15
-    yDim = int(f.readline())  # 10
-    mSpielfeld = [[0 for i in range(xDim)] for j in range(yDim)]
-    mSpielfeld [2][1] = 99
-    for row in range(yDim):
-        for col in range(xDim):
-            mSpielfeld[row][col] = int(f.read(1))
-        f.read(1)   # das NL fressen
-
-    f.close()
-    return [xDim, yDim, mSpielfeld]
-
-def waendeAufbauen(screen, blockSize):
-
-    [xD, yD, spielFeldWaende] = waendeLesen()
-    WEISS = (255, 255, 255)
-
-    #spielFeldWaende = [
-    #    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    #    [1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-    #    [1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1],
-    #    [1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1],
-    #    [1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1],
-    #    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    #]
-
-    # zaehlen, wie viele Punkte man maximal finden kann
-    maxPuntos = 0
-
-    for rIdx, rVal in enumerate(spielFeldWaende):
-        for cIdx, cVal in enumerate(rVal):
-            if spielFeldWaende[rIdx][cIdx] == 1:
-                pygame.draw.rect(screen, CYAN, [cIdx*blockSize, rIdx*blockSize, blockSize, blockSize], 0)
-            elif spielFeldWaende[rIdx][cIdx] == 0:
-                pygame.draw.circle(screen, WEISS, [cIdx*blockSize + 15, rIdx*blockSize + 15], 5, 0)
-                maxPuntos += 1
-    return [spielFeldWaende, maxPuntos]
-
-def baueDasSpielfeldAuf(captionString):
-    pygame.init()
-    screen = pygame.display.set_mode((450, 500))
-    pygame.display.set_caption(captionString)
-    # hier kommen dann die ganzen Waende
-    [spf, maxPuntos] = waendeAufbauen(screen, 30)
-    return [screen, spf, maxPuntos]
 
 window = turtle.Screen()
 window.bgcolor("black")
 window.title("Tiefensuche im Labyrinth")
 window.setup(1600,900)
-start_x = 15
-start_y = 10
-end_x = 15
-end_y = 10
+start_x = 10
+start_y = 33
+end_x = 33
+end_y = 32
 
 class Wall(turtle.Turtle):
     def __init__(self):
@@ -66,6 +16,7 @@ class Wall(turtle.Turtle):
         self.color("white")
         self.penup()
         self.speed(0)
+
 
 class Green(turtle.Turtle):
     def __init__(self):
@@ -82,6 +33,43 @@ class Red(turtle.Turtle):
         self.color("red")
         self.penup()
         self.speed(0)
+
+grid = [
+"1111111111111111111111111111111111",
+"1000000000000000000000000000000001",
+"1010111111111111100111111111110101",
+"1010100001000000100001000000010101",
+"1011101111111110111001011111010101",
+"1000001000000010001001000100010101",
+"1011101111111011101001110111011101",
+"1011101111111011101001110111011101",
+"1010101000001010001001010001000101",
+"1010111011111010111001011101110101",
+"1010000010000010100000000101000101",
+"1010111110101110111111111101011101",
+"1010100000101000000000000001010001",
+"1010101011101111111111011111010101",
+"1010101010100000000001010001010101",
+"1010101010111110111111010111011101",
+"1010101010100010100000000100000101",
+"1010101010101110111111110111110101",
+"1000101010101000000000010000010101",
+"1011101110101111111001110111110101",
+"1010000000100000000001000100010101",
+"1010111110111011111101110101110101",
+"1010100010001010000100010101000101",
+"1011101110111010100111011101110101",
+"1010001000101010100001010000010101",
+"1010111011101010111101110111010101",
+"1010100010100010000100000101000101",
+"1010101110111011100111011101111101",
+"1000100000101000100101010000000001",
+"1000100000101000100101010000000001",
+"1011101110101111111101010111110101",
+"1010001010000000000000010100010101",
+"101111101111111111111101110111111e",
+"0000000000s00000000000000000000100",
+"0000000000000000000000000000000000"]
 
 def paint_blob(x, y, blob):
     screen_x = -700 + (x * 24)
@@ -122,7 +110,7 @@ def _tiefensuche(visited, x, y):
     if y + 1 < 35 and grid[y + 1][x] != "0" and not visited[y+1][x]:
         _tiefensuche(visited, x, y + 1)
 
-def tiefensuche(grid):
+def tiefensuche():
     visited = []
     for i in range(len(grid)):
         l = []
